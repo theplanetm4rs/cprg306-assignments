0abf186fe743c3d3c95cd9325ecb01dc7f5ff0e9
Mariel Bananola <theplanetm4rs@gmail.com>
2026-01-29 08:37:29 -0700

    Header Changes
    small changes to the header of the main landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
+import NavBar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 
+    <div className="flex flex-col items-center justify-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="justify-center max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Welcome to my CPRG-306-Projects
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             All assignments for the Web Development 2 Class Winter 2026 Section B
           </p>
         </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
